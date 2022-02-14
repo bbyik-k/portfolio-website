38104312f33cb2b939d343f550c13fc0883ad5d7
bbyik <kby00517@naver.com>
2022-02-14 16:31:01 +0900

    Add js for navbar menu click

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+//Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  const scrollTo = document.querySelector(link);
+  if (link == null) {
+    return;
+  }
+  console.log(scrollTo);
+  scrollTo.scrollIntoView({
+    behavior: "smooth"
+  });
+});
